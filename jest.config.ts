9eb0f19e6e0fd82f611b1cbb62423149068c66a2
Stobles <ilovespnjensen@inbox.ru>
2024-01-20 11:36:01 +0300

    ci:fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -16,6 +16,8 @@ const config: Config = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+
+  modulePathIgnorePatterns: ["<rootDir>/tests/"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
